460d2bd5fecfc873c8220e58bbde91fb749789a8
xiaofei-wang <xiaofei.wang1987@gmail.com>
2019-03-07 15:12:55 -0500

    add 1ch

---
+++ egs/reverb/asr1_1ch/run.sh
@@ -0,0 +1,353 @@
+#!/bin/bash
+
+# Copyright 2018 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh
+. ./cmd.sh
+
+# general configuration
+backend=pytorch
+stage=0        # start from 0 if you need to start from data preparation
+ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
+debugmode=1
+N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
+verbose=0      # verbose option
+resume=        # Resume the training from snapshot
+stop_stage=4
+
+# configuration path
+preprocess_conf=conf/preprocess.json
+
+# network architecture
+use_beamformer=false
+use_beamformer_first=false
+use_wpe=true
+use_dnn_mask_for_wpe=true
+blayers=1
+wlayers=1
+btype=blstmp_1d
+
+# network architecture
+# encoder related
+etype=vggblstmp     # encoder architecture type
+elayers=3
+eunits=1024
+eprojs=1024
+subsample=1_2_2_1_1 # skip every n frame from input to nth layers
+# decoder related
+dlayers=1
+dunits=1024
+# attention related
+atype=location
+adim=1024
+aconv_chans=10
+aconv_filts=100
+
+# hybrid CTC/attention
+mtlalpha=0.5
+
+# minibatch related
+batchsize=18
+maxlen_in=600  # if input length  > maxlen_in, batchsize is automatically reduced
+maxlen_out=150 # if output length > maxlen_out, batchsize is automatically reduced
+
+# optimization related
+sortagrad=0 # Feed samples from shortest to longest ; -1: enabled for all epochs, 0: disabled, other: enabled for 'other' epochs
+opt=adadelta
+epochs=10
+patience=3
+
+# rnnlm related
+use_wordlm=true     # false means to train/use a character LM
+lm_vocabsize=65000  # effective only for word LMs
+lm_layers=1         # 2 for character LMs
+lm_units=1000       # 650 for character LMs
+lm_opt=sgd          # adam for character LMs
+lm_sortagrad=0 # Feed samples from shortest to longest ; -1: enabled for all epochs, 0: disabled, other: enabled for 'other' epochs
+lm_batchsize=300    # 1024 for character LMs
+lm_epochs=20        # number of epochs
+lm_patience=3
+lm_maxlen=40        # 150 for character LMs
+lm_resume=          # specify a snapshot file to resume LM training
+lmtag=              # tag for managing LMs
+
+# decoding parameter
+lm_weight=1.0
+beam_size=30
+penalty=0
+maxlenratio=0.0
+minlenratio=0.0
+ctc_weight=0.3
+recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
+
+# scheduled sampling option
+samp_prob=0.0
+
+# data
+reverb=/export/corpora5/REVERB_2014/REVERB    # JHU setup
+wsjcam0=/export/corpora3/LDC/LDC95S24/wsjcam0 # JHU setup
+wsj0=/export/corpora5/LDC/LDC93S6B            # JHU setup
+wsj1=/export/corpora5/LDC/LDC94S13B           # JHU setup
+wavdir=${PWD}/wav # place to store WAV files
+
+# exp tag
+tag="" # tag for managing experiments.
+
+. utils/parse_options.sh || exit 1;
+
+. ./path.sh
+. ./cmd.sh
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=tr_simu_1ch_si284
+train_dev=dt_multi_1ch
+recog_set="dt_real_1ch dt_simu_1ch et_real_1ch et_simu_1ch"
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    ### Task dependent. You have to make the following data preparation part by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    wavdir=${PWD}/wav # set the directory of the multi-condition training WAV files to be generated
+    echo "stage 0: Data preparation"
+    #local/generate_data.sh --wavdir ${wavdir} ${wsjcam0}
+    local/prepare_simu_data.sh --wavdir ${wavdir} ${reverb} ${wsjcam0}
+    local/prepare_real_data.sh --wavdir ${wavdir} ${reverb}
+    
+    # Additionally use WSJ clean data. Otherwise the encoder decoder is not well trained
+    local/wsj_data_prep.sh ${wsj0}/??-{?,??}.? ${wsj1}/??-{?,??}.?
+    local/wsj_format_data.sh
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    ### Task dependent. You have to design training and dev sets by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 1: Dump wav files into a HDF5 file"
+    # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
+    tasks="${recog_set} tr_simu_1ch"
+
+    # Note that data/tr05_multi_noisy_multich has multi-channel wav data, while data/train_si284 has 1ch only
+    dump_pcm.sh --nj 64 --filetype "sound.hdf5" data/train_si284
+    for setname in tr_simu_1ch ${recog_set}; do
+        dump_pcm.sh --nj 64 --filetype "sound.hdf5" data/${setname}
+    done
+    echo "combine real and simulation development data"
+    utils/combine_data.sh data/${train_dev} data/dt_real_1ch data/dt_simu_1ch
+    echo "combine reverb simulation and wsj clean training data"
+    utils/combine_data.sh data/${train_set} data/train_si284 data/tr_simu_1ch
+fi
+
+train_set="${train_set}"
+train_dev="${train_dev}"
+# Rename recog_set: e.g. dt05_real_isolated_6ch_track -> dt05_real_isolated_6ch_track_multich
+#recog_set="$(for setname in ${recog_set}; do echo -n "${setname}_multich "; done)"
+
+dict=data/lang_1char/${train_set}_units.txt
+echo "dictionary: ${dict}"
+nlsyms=data/lang_1char/non_lang_syms.txt
+
+if [ -z ${tag} ]; then
+    expname=${train_set}_${backend}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_d${dlayers}_unit${dunits}_${atype}${adim}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_sampprob${samp_prob}_bs$((batchsize * ngpu))_mli${maxlen_in}_mlo${maxlen_out}_usebf${use_beamformer}_usebff${use_beamformer_first}_${btype}_usewp${use_wpe}_usednnw${use_dnn_mask_for_wpe}
+else
+    expname=${train_set}_${backend}_${tag}
+fi
+expdir=exp/${expname}
+mkdir -p ${expdir}
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    ### Task dependent. You have to check non-linguistic symbols used in the corpus.
+    echo "stage 2: Dictionary and Json Data Preparation"
+    utils/combine_data.sh data/${train_set} data/train_si284 data/tr_simu_1ch
+    mkdir -p data/lang_1char/
+
+    echo "make a non-linguistic symbol list"
+    cut -f 2- data/${train_set}/text | tr " " "\n" | sort | uniq | grep "<" > ${nlsyms}
+    cat ${nlsyms}
+
+    echo "make a dictionary"
+    echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
+    | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
+    wc -l ${dict}
+
+    echo "make json files"
+    for setname in tr_simu_1ch ${train_dev} ${recog_set}; do
+        data2json.sh --cmd "${train_cmd}" --nj 30 \
+        --category "singlechannel" \
+        --preprocess-conf ${preprocess_conf} --filetype sound.hdf5 \
+        --feat data/${setname}/feats.scp --nlsyms ${nlsyms} \
+        --out data/${setname}/data.json data/${setname} ${dict}
+    done
+
+    setname=train_si284
+    data2json.sh --cmd "${train_cmd}" --nj 30 \
+    --category "singlechannel" \
+    --preprocess-conf ${preprocess_conf} --filetype sound.hdf5 \
+    --feat data/${setname}/feats.scp --nlsyms ${nlsyms} \
+    --out data/${setname}/data.json data/${setname} ${dict}
+
+    mkdir -p data/${train_set}
+    concatjson.py data/tr_simu_1ch/data.json data/train_si284/data.json > data/${train_set}/data.json
+fi
+
+# It takes a few days. If you just want to end-to-end ASR without LM,
+# you can skip this and remove --rnnlm option in the recognition (stage 5)
+if [ -z ${lmtag} ]; then
+    lmtag=${lm_layers}layer_unit${lm_units}_${lm_opt}_bs${lm_batchsize}
+    if [ ${use_wordlm} = true ]; then
+        lmtag=${lmtag}_word${lm_vocabsize}
+    fi
+fi
+lmexpname=train_rnnlm_${backend}_${lmtag}
+lmexpdir=exp/${lmexpname}
+mkdir -p ${lmexpdir}
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+    echo "stage 3: LM Preparation"
+    if [ ${use_wordlm} = true ]; then
+        lmdatadir=data/local/wordlm_train
+        lmdict=${lmdatadir}/wordlist_${lm_vocabsize}.txt
+        mkdir -p ${lmdatadir}
+        cut -f 2- -d" " data/${train_set}/text > ${lmdatadir}/train_trans.txt
+        zcat ${wsj1}/13-32.1/wsj1/doc/lng_modl/lm_train/np_data/{87,88,89}/*.z \
+                | grep -v "<" | tr "[:lower:]" "[:upper:]" > ${lmdatadir}/train_others.txt
+        cut -f 2- -d" " data/${train_dev}/text > ${lmdatadir}/valid.txt
+        cat ${lmdatadir}/train_trans.txt ${lmdatadir}/train_others.txt > ${lmdatadir}/train.txt
+        text2vocabulary.py -s ${lm_vocabsize} -o ${lmdict} ${lmdatadir}/train.txt
+    else
+        lmdatadir=data/local/lm_train
+        lmdict=${dict}
+        mkdir -p ${lmdatadir}
+        text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text \
+            | cut -f 2- -d" " > ${lmdatadir}/train_trans.txt
+        zcat ${wsj1}/13-32.1/wsj1/doc/lng_modl/lm_train/np_data/{87,88,89}/*.z \
+            | grep -v "<" | tr "[:lower:]" "[:upper:]" \
+            | text2token.py -n 1 | cut -f 2- -d" " > ${lmdatadir}/train_others.txt
+        text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_dev}/text \
+            | cut -f 2- -d" " > ${lmdatadir}/valid.txt
+        cat ${lmdatadir}/train_trans.txt ${lmdatadir}/train_others.txt > ${lmdatadir}/train.txt
+    fi
+    # use only 1 gpu
+    if [ ${ngpu} -gt 1 ]; then
+	echo "LM training does not support multi-gpu. single gpu will be used."
+    fi
+    ${cuda_cmd} --gpu ${ngpu} ${lmexpdir}/train.log \
+        lm_train.py \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --verbose 1 \
+        --outdir ${lmexpdir} \
+        --tensorboard-dir tensorboard/${lmexpname} \
+        --train-label ${lmdatadir}/train.txt \
+        --valid-label ${lmdatadir}/valid.txt \
+        --resume ${lm_resume} \
+        --layer ${lm_layers} \
+        --unit ${lm_units} \
+        --opt ${lm_opt} \
+        --sortagrad ${lm_sortagrad} \
+        --batchsize ${lm_batchsize} \
+        --epoch ${lm_epochs} \
+        --patience ${lm_patience} \
+        --maxlen ${lm_maxlen} \
+        --dict ${lmdict}
+fi
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    echo "stage 4: Network Training: expdir=${expdir}"
+
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        asr_train.py \
+        --use-frontend True \
+        --use-beamformer ${use_beamformer} \
+        --use-beamformer_first ${use_beamformer_first} \
+        --use-wpe ${use_wpe} \
+        --use-dnn-mask-for-wpe ${use_dnn_mask_for_wpe} \
+        --blayers ${blayers} \
+        --btype ${btype} \
+        --ref-channel 1 \
+        --wlayers ${wlayers} \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --tensorboard-dir tensorboard/${expname} \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json data/${train_set}/data.json \
+        --valid-json data/${train_dev}/data.json \
+        --preprocess-conf ${preprocess_conf} \
+        --etype ${etype} \
+        --elayers ${elayers} \
+        --eunits ${eunits} \
+        --eprojs ${eprojs} \
+        --subsample ${subsample} \
+        --dlayers ${dlayers} \
+        --dunits ${dunits} \
+        --atype ${atype} \
+        --adim ${adim} \
+        --aconv-chans ${aconv_chans} \
+        --aconv-filts ${aconv_filts} \
+        --mtlalpha ${mtlalpha} \
+        --batch-size ${batchsize} \
+        --maxlen-in ${maxlen_in} \
+        --sampling-probability ${samp_prob} \
+        --maxlen-out ${maxlen_out} \
+        --opt ${opt} \
+        --sortagrad ${sortagrad} \
+        --epochs ${epochs} \
+        --patience ${patience}
+fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo "stage 5: Decoding"
+    nj=32
+
+    for rtask in ${recog_set}; do
+    (
+        decode_dir=decode_${rtask}_beam${beam_size}_e${recog_model}_p${penalty}_len${minlenratio}-${maxlenratio}_ctcw${ctc_weight}_rnnlm${lm_weight}_${lmtag}
+        if [ ${use_wordlm} = true ]; then
+            recog_opts="--word-rnnlm ${lmexpdir}/rnnlm.model.best"
+        else
+            recog_opts="--rnnlm ${lmexpdir}/rnnlm.model.best"
+        fi
+
+        # split data
+        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json 
+
+        #### use CPU for decoding
+        ngpu=0
+
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            asr_recog.py \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --debugmode ${debugmode} \
+            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/results/${recog_model}  \
+            --beam-size ${beam_size} \
+            --penalty ${penalty} \
+            --maxlenratio ${maxlenratio} \
+            --minlenratio ${minlenratio} \
+            --ctc-weight ${ctc_weight} \
+            --lm-weight ${lm_weight} \
+            ${recog_opts} &
+        wait
+
+        score_sclite.sh --wer true --nlsyms ${nlsyms} ${expdir}/${decode_dir} ${dict}
+
+    ) &
+    done
+    wait
+    echo "Report the result"
+    decode_part_dir=beam${beam_size}_e${recog_model}_p${penalty}_len${minlenratio}-${maxlenratio}_ctcw${ctc_weight}_rnnlm${lm_weight}_${lmtag}
+    local/get_results.sh ${nlsyms} ${dict} ${expdir} ${decode_part_dir}
+    echo "Finished"
+fi
